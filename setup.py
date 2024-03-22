6de36a8e258fe0b24b3fea3e68364e00bbd970cd
rohitspatil30 <rohitspatil30@gmail.com>
2024-03-23 00:02:39 +0530

    Files updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+import langchain_openai
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Rohit Patil',
+    author_email='rohitspatil30@gmail.com',
+    install_requires=["openai", "langchain",
+                      "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+)
